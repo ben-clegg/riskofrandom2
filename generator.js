5a89e06d3cecbe7bad59963dfd695d16360ff8ed
Ben Clegg <bclegg@midcode.co.uk>
2020-01-04 16:05:46 +0000

    added random blurred backgrounds

--- generator.js
+++ generator.js
@@ -10,7 +10,22 @@ var survivors = Array(
     { name: "REX"}
 );
 
+var backgrounds = Array(
+    "acres.png",
+    "aqueduct.png",
+    "aspect.png",
+    "call.png",
+    "delta.png",
+    "depths.png",
+    "plains.png",
+    "roost.png"
+);
+
 var chosenSurvivor = survivors[Math.floor(Math.random() * survivors.length)]
+var chosenBG = "assets/bg/" + backgrounds[Math.floor(Math.random() * backgrounds.length)]
 
 document.getElementById("survivor").innerHTML = chosenSurvivor.name;
 
+var body = document.getElementsByTagName('body')[0];
+body.style.background = "url(" + chosenBG + ")" + "no-repeat center center fixed";
+body.style.backgroundSize = "cover";
